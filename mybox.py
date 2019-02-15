e9cfa28ac90df5f5d63ce9b804cf2e75a8a0a139
Yerkezhan91 <47048442+Yerkezhan91@users.noreply.github.com>
2019-02-15 13:20:41 +0600

    Update mybox.py

--- mybox.py
+++ mybox.py
@@ -1,3 +1,24 @@
+from myclass import Student
 class MyBox:
-    def __init__(self)
-    
+    def __init__(self):
+        self.color=clr
+        self.weidth=w
+        self.height=h
+
+    def __len__(self,l):
+        self.l=len(self.color)
+    def __add__(self,b):
+        self.MyBox.append(b)
+        self.b=5
+    def __remove__(self,h):
+        assert h in self.MyBox
+        val=self.MyBox.val(h)
+        return self.MyBox.pop(val)
+    def __contains__(self,w):
+        return w in self .MyBox
+    def _iter__(self, w):
+        return  MYBox(self.w)
+
+MyBox=Student()
+print(MyBox.b)
+print(MyBox.l)
